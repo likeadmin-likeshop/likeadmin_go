c998ba180d3cce0594923b83428d5ad0829e1e39
geeknonerd <geek@best2geek.com>
2022-12-27 21:02:45 +0800

    feat: 优化gorm日志配置

--- server/core/db.go
+++ server/core/db.go
@@ -3,9 +3,11 @@ package core
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 	"likeadmin/config"
 	"log"
+	"os"
 	"time"
 )
 
@@ -13,6 +15,23 @@ var DB = initMysql()
 
 //initMysql 初始化mysql会话
 func initMysql() *gorm.DB {
+	// 日志配置
+	slowThreshold := time.Second
+	ignoreRecordNotFoundError := true
+	if config.Config.GinMode == "debug" {
+		slowThreshold = 200 * time.Millisecond
+		ignoreRecordNotFoundError = false
+	}
+	logger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             slowThreshold,             // 慢 SQL 阈值
+			LogLevel:                  logger.Warn,               // 日志级别
+			IgnoreRecordNotFoundError: ignoreRecordNotFoundError, // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,                      // 彩色打印
+		},
+	)
+	// 初始化会话
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       config.Config.DatabaseUrl,         // DSN data source name
 		DefaultStringSize:         config.Config.DbDefaultStringSize, // string 类型字段的默认长度
@@ -23,7 +42,8 @@ func initMysql() *gorm.DB {
 			TablePrefix:   config.Config.DbTablePrefix, // 表名前缀
 			SingularTable: true,                        // 使用单一表名, eg. `User` => `user`
 		},
-		DisableForeignKeyConstraintWhenMigrating: true, // 禁用自动创建外键约束
+		DisableForeignKeyConstraintWhenMigrating: true,   // 禁用自动创建外键约束
+		Logger:                                   logger, // 自定义Logger
 	})
 	if err != nil {
 		log.Fatal("initMysql gorm.Open err:", err)
