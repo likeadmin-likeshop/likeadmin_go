a1bf4a98369eb11ce950f1b392c46a75203fb957
geeknonerd <geek@best2geek.com>
2023-01-11 15:06:53 +0800

    fix: 调整"管理员新增"和"管理员编辑"参数校验

--- server/admin/schemas/req/system.go
+++ server/admin/schemas/req/system.go
@@ -35,8 +35,8 @@ type SystemAuthAdminAddReq struct {
 	Nickname     string `form:"nickname" binding:"required,min=2,max=30"` // 昵称
 	Password     string `form:"password" binding:"required"`              // 密码
 	Avatar       string `form:"avatar" binding:"required"`                // 头像
-	Role         uint   `form:"role" binding:"required,gt=0"`             // 角色
-	Sort         int    `form:"sort" binding:"required,gte=0"`            // 排序
+	Role         uint   `form:"role" binding:"gte=0"`                     // 角色
+	Sort         int    `form:"sort" binding:"gte=0"`                     // 排序
 	IsDisable    uint8  `form:"isDisable" binding:"oneof=0 1"`            // 是否禁用: [0=否, 1=是]
 	IsMultipoint uint8  `form:"isMultipoint" binding:"oneof=0 1"`         // 多端登录: [0=否, 1=是]
 }
@@ -48,10 +48,10 @@ type SystemAuthAdminEditReq struct {
 	PostId       uint   `form:"postId" binding:"required,gt=0"`           // 岗位ID
 	Username     string `form:"username" binding:"required,min=2,max=20"` // 账号
 	Nickname     string `form:"nickname" binding:"required,min=2,max=30"` // 昵称
-	Password     string `form:"password" binding:"required"`              // 密码
+	Password     string `form:"password"`                                 // 密码
 	Avatar       string `form:"avatar"`                                   // 头像
-	Role         uint   `form:"role" binding:"required,gt=0"`             // 角色
-	Sort         int    `form:"sort" binding:"required,gte=0"`            // 排序
+	Role         uint   `form:"role" binding:"gte=0"`                     // 角色
+	Sort         int    `form:"sort" binding:"gte=0"`                     // 排序
 	IsDisable    uint8  `form:"isDisable" binding:"oneof=0 1"`            // 是否禁用: [0=否, 1=是]
 	IsMultipoint uint8  `form:"isMultipoint" binding:"oneof=0 1"`         // 多端登录: [0=否, 1=是]
 }
